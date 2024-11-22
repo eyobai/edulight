dc3ec9981340f7d809d966a9960b292c115c8b6d
Eyob <eyobsisayy@gmail.com>
2024-11-22 15:40:17 +0300

    hive_flutter added

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,12 @@ import 'package:edulight/views/auth/login_screen.dart';
 import 'package:edulight/views/auth/register_screen.dart';
 import 'package:edulight/views/main/home_screen.dart';
 import 'package:edulight/models/user_provider.dart';
-//import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
-  // // Initialize Hive
-  // await Hive.initFlutter();
-  // await Hive.openBox('userBox');
+  // Initialize Hive
+  await Hive.initFlutter();
+  await Hive.openBox('userBox');
 
   runApp(
     MultiProvider(
@@ -25,13 +25,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Check login state
-    // var box = Hive.box('userBox');
-    // bool isLoggedIn = box.get('isLoggedIn', defaultValue: false);
+    var box = Hive.box('userBox');
+    bool isLoggedIn = box.get('isLoggedIn', defaultValue: false);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Your App',
-      //  initialRoute: isLoggedIn ? '/home' : '/',
+      initialRoute: isLoggedIn ? '/home' : '/',
       routes: {
         '/': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
