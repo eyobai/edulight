40821e6f84d0c041564bf1a3d9967e4bb80443bf
Eyob <eyobsisayy@gmail.com>
2025-02-05 22:06:19 +0300

    student model added

--- lib/models/student_model.dart
+++ lib/models/student_model.dart
@@ -2,19 +2,24 @@ class StudentModel {
   final int id;
   final String name;
   final String email;
+  final String token;
 
   StudentModel({
     required this.id,
     required this.name,
     required this.email,
+    required this.token,
   });
 
   // Factory method to create a StudentModel from JSON
   factory StudentModel.fromJson(Map<String, dynamic> json) {
+    // Extract the first student entry
+    final studentData = json['user']['student'][0];
     return StudentModel(
-      id: json['user']['id'],
+      id: studentData['id'], // Use the id from the student array
       name: json['user']['name'],
       email: json['user']['email'],
+      token: json['token'],
     );
   }
 
@@ -24,6 +29,7 @@ class StudentModel {
       'id': id,
       'name': name,
       'email': email,
+      'token': token,
     };
   }
 }
