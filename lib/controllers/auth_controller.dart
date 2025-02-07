4fba904f1c050638aa8a04b7ee43b0d6cf522a17
Eyob <eyobsisayy@gmail.com>
2025-02-07 12:10:38 +0300

    email authentication changed to phone

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,25 +1,21 @@
 import 'dart:convert';
 import 'package:edulight/models/api_service.dart';
 import 'package:edulight/models/student_model.dart'; // Import your student model
+import 'package:hive/hive.dart';
+import 'package:http/http.dart' as http; // Add this import
 
 class AuthController {
   final ApiService _apiService = ApiService();
 
   Future<StudentModel?> login(String email, String password) async {
     try {
-      // Call the API service to perform login
-      final response = await _apiService.login(email, password);
-      if (response.statusCode == 200) {
-        // Parse the response and return a StudentModel
-        final Map<String, dynamic> responseData = jsonDecode(response.body);
-        print('JSON Response: ${response.body}');
-        return StudentModel.fromJson(responseData);
+      final data = await _apiService.login(email, password);
+      if (data != null && data.containsKey('user')) {
+        return StudentModel.fromJson(data);
       } else {
-        // Handle login failure
-        throw Exception('Login failed: ${response.reasonPhrase}');
+        throw Exception('User data not found in response');
       }
     } catch (e) {
-      // Handle any exceptions
       print('Error during login: $e');
       return null;
     }
@@ -33,7 +29,7 @@ class AuthController {
     required String passwordConfirmation,
   }) async {
     try {
-      final response = await _apiService.register(
+      await _apiService.register(
         name: name,
         email: email,
         phone: phone,
@@ -41,18 +37,35 @@ class AuthController {
         passwordConfirmation: passwordConfirmation,
       );
 
-      if (response.statusCode == 201) {
+      var box = Hive.box('userBox');
+      String? token = box.get('token');
+
+      if (token != null) {
         // Registration was successful
         print('Registration successful');
         return true;
       } else {
-        // Handle other status codes or errors
-        print('Registration failed: ${response.body}');
+        // Handle registration failure
+        print('Registration failed: Token not found');
         return false;
       }
     } catch (e) {
       print('Error during registration: $e');
       return false;
+    }
+  }
+
+  Future<StudentModel?> loginWithPhone(String phone, String password) async {
+    try {
+      final data = await _apiService.login(phone, password);
+      if (data != null && data.containsKey('user')) {
+        return StudentModel.fromJson(data);
+      } else {
+        throw Exception('User data not found in response');
+      }
+    } catch (e) {
+      print('Error during login with phone: $e');
+      return null;
     }
   }
 }
