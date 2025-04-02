2bee4bd0657d38d4862f10c5a441913cefdeaa4f
Eyob <eyobsisayy@gmail.com>
2025-04-02 21:16:20 +0300

    state managment added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:edulight/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:edulight/views/auth/login_screen.dart';
@@ -13,14 +14,30 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox('userBox');
 
+  // Check and restore user data
+  var box = Hive.box('userBox');
+  final isLoggedIn = box.get('isLoggedIn', defaultValue: false);
+  final userProvider = UserProvider();
+
+  if (isLoggedIn) {
+    // Restore user data to provider
+    userProvider.setUser(
+      User(
+        email: box.get('userEmail', defaultValue: ''),
+        name: box.get('userName', defaultValue: ''),
+        userId: box.get('userId', defaultValue: ''),
+      ),
+    );
+  }
+
   runApp(
     MultiProvider(
       providers: [
         Provider<ApiService>(
           create: (_) => ApiService(),
         ),
-        ChangeNotifierProvider<UserProvider>(
-          create: (_) => UserProvider(),
+        ChangeNotifierProvider<UserProvider>.value(
+          value: userProvider,
         ),
       ],
       child: MyApp(),
@@ -31,23 +48,25 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Check login state
-    var box = Hive.box('userBox');
-    bool isLoggedIn = box.get('isLoggedIn', defaultValue: false);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EduLight',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: isLoggedIn ? '/home' : '/',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/home': (context) => HomeScreen(),
-        '/subscription': (context) => SubscriptionScreen(),
-      },
+      home: Consumer<UserProvider>(
+        builder: (context, userProvider, child) {
+          var box = Hive.box('userBox');
+          bool isLoggedIn = box.get('isLoggedIn', defaultValue: false);
+
+          // If logged in and user data exists, go to HomeScreen
+          if (isLoggedIn && userProvider.user != null) {
+            return HomeScreen();
+          }
+          // Otherwise, go to LoginScreen
+          return LoginScreen();
+        },
+      ),
     );
   }
 }
